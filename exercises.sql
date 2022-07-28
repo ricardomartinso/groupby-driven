1ad097952909480ace3b0cb895abfa581c5782a7
Ricardo <ricardomartinso.dev@gmail.com>
2022-07-28 10:06:55 -0300

    adjust exercise 4

--- exercises.sql
+++ exercises.sql
@@ -1,2 +1,8 @@
 SELECT COUNT(experiences."endDate") AS "currentExperiences" FROM experiences;
 
+ SELECT u.id, COUNT(e."courseId") AS educations FROM educations e JOIN users u ON u.id = e."userId" GROUP BY u.id;
+
+SELECT users.name, COUNT(testimonials."writerId") AS testimonials FROM users JOIN testimonials ON users.id = testimonials."writerId" GROUP BY users.id;
+
+SELECT MAX(j.salary) AS "maximumSalary", r.name AS role FROM roles r JOIN jobs j ON r.id = j."roleId" WHERE j.active = true GROUP BY r.name ORDER BY r.name ASC;
+
